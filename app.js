9b6f2109641a42b80a6b8037b3da49aa0f490c32
Mongkii <mongkii@hotmail.com>
2024-02-15 22:35:51 +0800

    feat: init commit

--- app.js
+++ app.js
@@ -1,156 +1,92 @@
-const express = require('express');
-const leapcell = require('@leapcell/leapcell-js');
-const showdown   = require('showdown');
-const dayjs = require('dayjs');
+import express from 'express';
+import { Leapcell } from '@leapcell/leapcell-js';
+import axios from 'axios';
+import { load } from 'cheerio';
+import 'dotenv/config';
+
+const configs = [
+  {
+    emulator: 'RetroArch',
+    url: 'https://buildbot.libretro.com/stable',
+    parse: ($) => {
+      const regNum = /^\d+/;
+      const versions = $('td.fb-n')
+        .map((i, ele) => $(ele).text().trim())
+        .toArray()
+        .filter((item) => regNum.test(item));
+
+      return versions.map((version) => ({
+        url: `https://buildbot.libretro.com/stable/${version}/android/RetroArch_aarch64.apk`,
+        version,
+      }));
+    },
+  },
+  {
+    emulator: 'Dolphin',
+    url: 'https://dolphin-emu.org/download/?nocr=true',
+    parse: ($) => {
+      const infoTrs = $('#download-beta table.versions-list.dev-versions tr.infos');
+
+      return infoTrs
+        .map((i, ele) => {
+          const version = $(ele).find('td.version').text().trim();
+          const url = $(ele).next().find('a.android').attr('href');
+          return { url, version };
+        })
+        .toArray();
+    },
+  },
+];
 
 const app = express();
 app.set('views', './templates');
 app.set('view engine', 'ejs');
 
-const api = new leapcell.Leapcell({
-    apiKey: process.env.LEAPCELL_API_KEY,
+const api = new Leapcell({
+  apiKey: process.env.LEAPCELL_API_KEY,
+});
+const repo = process.env.RESOURCE;
+const tableId = process.env.TABLE_ID;
+
+const table = api.repo(repo).table(tableId);
+
+app.get('/update-list', async (request, response) => {
+  for (let i = 0, len = configs.length; i < len; i++) {
+    const { url, parse, emulator } = configs[i];
+
+    const pageResp = await axios.get(url);
+    const result = parse(load(pageResp.data));
+
+    await table.records.deleteMany({ where: { Emulator: { eq: emulator } } });
+    await table.records.createMany(
+      result.map(({ version, url }) => ({ Version: version, Emulator: emulator, URL: url }))
+    );
+  }
+
+  response.send('OK');
 });
 
-const resource = process.env.RESOURCE || "issac/flask-blog";
-const tableId = process.env.TABLE_ID || "tbl1738878922167070720";
-const author = process.env.AUTHOR || "Leapcell User";
-const avatar = process.env.AVATAR || "https://leapcell.io/logo.png";
+app.get('/list/:emulator', async (request, response) => {
+  const rawEmulator = request.params.emulator;
 
-const table = api.repo(resource).table(tableId);
+  let emulator;
+  if (rawEmulator === 'retroarch') {
+    emulator = 'RetroArch';
+  } else if (rawEmulator === 'dolphin') {
+    emulator = 'Dolphin';
+  } else {
+    return response.status(400).send('Invalid emulator');
+  }
 
+  const records = await table.records.findMany({ where: { Emulator: { eq: emulator } } });
+  const packages = records.map((record) => ({
+    url: record.fields['URL'],
+    version: record.fields['Version'],
+  }));
 
-app.get('/', async (request, response) => {
-    let res = [];
-    try {
-        res = await table.records.findMany();
-    } catch (error) {
-        console.log(error);
-        res = [];
-    }
-
-    const posts = res.map((post) => {
-        return {
-            record_id: post.record_id,
-            title: post.fields["title"],
-            content: post.fields["content"],
-            cover: post.fields["cover"] ? post.fields["cover"][0] ? post.fields["cover"][0] : "" : "",
-            category: post.fields["category"] || [],
-            create_time: dayjs.unix(post.create_time).format("MMMM DD, YYYY"),
-            summary: post.fields["content"] ? post.fields["content"].substring(0, 200) + "..." : "",
-
-        };
-    })
-
-    const list_posts = posts.slice(0, posts.length)
-    const params = {
-        "author": author,
-        "avatar": avatar,
-        "posts": list_posts,
-        category: null,
-        query: null,
-    }
-    return response.render('index', params);
-});
-
-app.get('/category/:category', async (request, response) => {
-    category = request.params.category;
-    let res = [];
-    try {
-        res = await table.records.findMany({
-            where: {
-                "category": {
-                    "contain": category
-                }
-            }
-        });
-    } catch (error) {
-        console.log(error);
-        res = [];
-    }
-
-    const posts = res.map((post) => {
-        return {
-            record_id: post.record_id,
-            title: post.fields["title"],
-            content: post.fields["content"],
-            cover: post.fields["cover"][0] ? post.fields["cover"][0] ? post.fields["cover"][0] : "" : "",
-            category: post.fields["category"] || [],
-            create_time: dayjs.unix(post.create_time).format("MMMM DD, YYYY"),
-            summary: post.fields["content"] ? post.fields["content"].substring(0, 200) + "..." : "",
-
-        };
-    })
-    const list_posts = posts.slice(0, posts.length);
-    const params = {
-        "author": author,
-        "avatar": avatar,
-        "posts": list_posts,
-        category: category,
-        query: null,
-    }
-    return response.render('index', params);
-});
-
-app.get('/search', async (request, response) => {
-    query = request.query.query;
-    let res = [];
-    try {
-        res = await table.records.search({
-            query: query,
-            search_fields: ["title", "content"],
-        });
-    } catch (error) {
-        console.log(error);
-        res = [];
-    }
-
-    const posts = res.map((post) => {
-        return {
-            record_id: post.record_id,
-            title: post.fields["title"],
-            content: post.fields["content"],
-            cover: post.fields["cover"][0] ? post.fields["cover"][0] ? post.fields["cover"][0] : "" : "",
-            category: post.fields["category"] || [],
-            create_time: dayjs.unix(post.create_time).format("MMMM DD, YYYY"),
-            summary: post.fields["content"] ? post.fields["content"].substring(0, 200) + "..." : "",
-
-        };
-    })
-    const list_posts = posts.slice(0, posts.length);
-    const params = {
-        "author": author,
-        "avatar": avatar,
-        "posts": list_posts,
-        category: null,
-        query: query,
-    }
-    return response.render('index', params);
-});
-
-app.get("/post/:post_id", async (request, response) => {
-    const post_id = request.params.post_id;
-    const res = await table.records.findById(post_id);
-    converter = new showdown.Converter();
-    const post = {
-        record_id: res.record_id,
-        title: res.fields["title"],
-        category: res.fields["category"] || [],
-        create_time: dayjs.unix(res.create_time).format("MMMM DD, YYYY"),
-        cover: res.fields["cover"] ? res.fields["cover"][0] ? res.fields["cover"][0] : "" : "",
-    };
-    return response.render('post', {
-        "author": author,
-        "avatar": avatar,
-        "post": post,
-        "markdown_html": converter.makeHtml(res.fields["content"] || ""),
-        "category": res.fields["category"] || [],
-    });
-});
-
-app.get("/hello", (request, response) => {
-    return response.send("Hello World");
+  return response.render('packages', { emulator, packages });
 });
 
 app.listen(8080, () => {
-    console.log('App is listening on port 8080');
+  console.log('App is listening on port 8080');
 });
